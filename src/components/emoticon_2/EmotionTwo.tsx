87b717c2aedb3959e21706e685f28d031e56a8a6
Scott | elbDev <sc@elbdev.de>
2023-07-25 18:04:52 +0200

    pushed my files into my branch

--- src/components/emoticon_2/EmotionTwo.tsx
+++ src/components/emoticon_2/EmotionTwo.tsx
@@ -1,9 +1,11 @@
 import { ReactElement, useState } from "react";
-import Table from "./Table";
+// import Table from "./Table";
 import "./EmoticonTwo.css";
 import { useRecoilState } from "recoil";
-import { partyItemsState } from "../../states/PartyItemState";
+// import { partyItemsState } from "../../states/PartyItemState";
 import * as React from 'react';
+
+
 
 export type EmoticonTwo = {
   firstName: string;
@@ -17,59 +19,61 @@ export default function EmoticonTwo({
   firstName,
   lastName,
 }: EmoticonTwo): ReactElement {
-  const [partyItems, setPartyItems] = useRecoilState(partyItemsState);
-  const [inputName, setInputName] = useState("");
-  const [inputItem, setInputItem] = useState("");
+  const [videoId, setVideoId] = useState("");
+  // const videoUrl = `https://www.youtube.com/embed/${videoId}`; 
+  // console.log(videoUrl);
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setVideoId(event.target.value);
+  };
   return (
     <>
+      {/* <img
+          src=""
+          className={"photobook"}
+          alt="background"
+          style={{
+            opacity: 1,
+            width: "100%",
+          }}
+          /> */}
       <div
         style={{
+          backgroundImage: `url('/nostalgia.jpg')`,
+          backgroundRepeat: 'no-repeat',
           padding: "2px",
           width: "100%",
           height: "1500px",
-          backgroundColor: "yellow",
         }}
       >
         <div style={{ padding: "70px" }}>
           <h1>
-            Willkommen, {firstName} {lastName} was bringst du zur Grillparty
-            mit?
+            Willkommen, {firstName} {lastName} In welche Zeit möchtest du zurückversetzt werden?
           </h1>
           <form>
-            <label>
-              Name:
-              <input
-                type="text"
-                value={inputName}
-                onChange={(e) => setInputName(e.target.value)}
-                name="name"
-              />
-            </label>
-            <label>
-              Ich bringe mit...
-              <input
-                type="text"
-                value={inputItem}
-                onChange={(e) => setInputItem(e.target.value)}
-                name="item"
-              />
-            </label>
-            <button
-              className="button-one"
-              onClick={() => {
-                if (inputItem && inputName) {
-                  setPartyItems((prev) => [
-                    ...prev,
-                    { name: inputName, item: inputItem },
-                  ]);
-                }
-              }}
-            >
-              Speichern
-            </button>
+            <div>
+              <select value={videoId} onChange={handleChange}>
+                <option value="">Keine Zeit ausgewählt</option>
+                <option value="Qt2mbGP6vFI">80er</option>
+                <option value="4fndeDfaWCg">90er</option>
+                <option value="LOZuxwVk7TU">2000er</option>
+              </select>
+              <p>Sie haben ausgewählt: {videoId}</p>
+            </div>
           </form>
+          {videoId !== "" && <div className="youtube-video-container">
+            <iframe className="videoNostalgie"
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${videoId}`}
+              title="YouTube video player"
+              // frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              // allowfullscreen
+            ></iframe>
+          </div>}
+          
           <div className="table" style={{ display: "flex" }}>
-            <Table items={partyItems}></Table>
+            {/* <Table items={partyItems}></Table> */}
           </div>
         </div>
       </div>
--- src/components/emoticon_2/Table.tsx
+++ src/components/emoticon_2/Table.tsx
@@ -6,19 +6,19 @@ export type TableProps = {
   items: PartyItem[];
 };
 
-export default function Table({ items }: TableProps): ReactElement {
-  return (
-    <div className="table-container">
-      <div className="table-row heading">
-        <div className="row-item">Wer?</div>
-        <div className="row-item">Was?</div>
-      </div>
-      {items.map((item) => (
-        <div className="table-row">
-          <div className="row-item">{item.name}</div>
-          <div className="row-item">{item.item}</div>
-        </div>
-      ))}
-    </div>
-  );
-}
+// export default function Table({ items }: TableProps): ReactElement {
+//   return (
+//     <div className="table-container">
+//       <div className="table-row heading">
+//         <div className="row-item">Wer?</div>
+//         <div className="row-item">Was?</div>
+//       </div>
+//       {items.map((item) => (
+//         <div className="table-row">
+//           <div className="row-item">{item.name}</div>
+//           <div className="row-item">{item.item}</div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
